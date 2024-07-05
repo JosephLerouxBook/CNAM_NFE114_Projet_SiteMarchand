865f08cf3941808f47dc2e9fd6e9eac78d3e67ec
Joseph Leroux <41457887+JosephLerouxBook@users.noreply.github.com>
2024-07-05 19:08:59 +0200

    Correction formulaire + navbar

--- index.php
+++ index.php
@@ -21,25 +21,14 @@
     <ul class="menu-main" style="position: static;">
         <li id="view" class="has-submenu is-selected is-expanded">
             <a href="php/display.php?inf=outils">Outils</a>
-            <ul class="submenu" style="max-height: 185px;">
-                <li class="is-selected">
-                    <a>Alésoirs</a>
-                </li>
-                <li>
-                    <a>Tarauds</a>
-                </li>
-                <li>
-                    <a>Lames de scie</a>
-                </li>
-            
-            </ul>
+
         </li>
 
 
     <li id="services" class="has-submenu">
         <a>Bobines</a>
     </li>
-
+<!--
     <li id="services" class="has-submenu">
         <a>Cartouches d'imprimantes</a>
     </li>
@@ -50,14 +39,7 @@
 
     <li id="view" class="has-submenu is-selected is-expanded">
         <a>Disques troncs, meules</a>
-        <ul class="submenu" style="max-height: 185px;">
-            <li class="is-selected">
-                <a>Meuler</a>
-            </li>
-            <li>
-                <a>Troçonner</a>
-            </li>
-        </ul>
+
     </li>
 
     <li id="services" class="has-submenu">
@@ -66,99 +48,21 @@
 
     <li id="view" class="has-submenu is-selected is-expanded">
         <a>Forets</a>
-        <ul class="submenu" style="max-height: 500px;">
-            <li class="is-selected">
-                <a>ARS</a>
-            </li>
-            <li>
-                <a>Queue cylindrique</a>
-            </li>
-            <li>
-                <a>Hélicoidal</a>
-            </li>
-            <li>
-                <a>Queue conique</a>
-            </li>
-            <li>
-                <a>A pointer</a>
-            </li>
-            <li>
-                <a>A centrer</a>
-            </li>
-            <li>
-                <a>Autres</a>
-            </li>
-        </ul>
     </li>
 
     <li id="view" class="has-submenu is-selected is-expanded">
         <a>Matériel informatique</a>
-        <ul class="submenu" style="max-height: 185px;">
-            <li class="is-selected">
-                <a>Câbles</a>
-            </li>
-            <li>
-                <a>Autres</a>
-            </li>    
-        </ul>
+
     </li>
 
     <li id="view" class="has-submenu is-selected is-expanded">
         <a>Matières</a>
-        <ul class="submenu" style="max-height: 500px;">
-            <li class="is-selected">
-                <a>XC48</a>
-            </li>
-            <li>
-                <a>AU4G</a>
-            </li>
-            <li>
-                <a>Bronze</a>
-            </li>
-            <li>
-                <a>Cuivre</a>
-            </li>
-            <li>
-                <a>ADX Laminé</a>
-            </li>
-            <li>
-                <a>A centrer</a>
-            </li>
-            <li>
-                <a>Acier Doux</a>
-            </li>
-            <li>
-                <a>XC38</a>
-            </li>
-            <li>
-                <a>A60</a>
-            </li>
-            <li>
-                <a>Stub</a>
-            </li>
-            <li>
-                <a>Laiton</a>
-            </li>
-            <li>
-                <a>Autres</a>
-            </li>
-        </ul>
+
     </li>
 
     <li id="view" class="has-submenu is-selected is-expanded">
         <a>Papier de verre, toile</a>
-        <ul class="submenu" style="max-height: 185px;">
-            <li class="is-selected">
-                <a>Feuille</a>
-            </li>
-            <li>
-                <a>Papier</a>
-            </li>
-            <li>
-                <a>Toile</a>
-            </li>
-        
-        </ul>
+
     </li>
 
     <li id="services" class="has-submenu">
@@ -168,7 +72,7 @@
     <li id="services" class="has-submenu">
         <a>Tiges filetées</a>
     </li>
-
+-->
     </div>
     </ul>
 
